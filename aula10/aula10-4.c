fff5cdcc686805da3adaa242ce641b71201d37a0
Danilo Abreu <daniloabreu1@gmail.com>
2019-03-29 12:26:17 -0300

    fib

---
+++ aula10/aula10-4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int fibonacci(int f){
+    int s=0;
+    if(f<2){
+        return f;
+    }else{
+        return fibonacci(f-1)+fibonacci(f-2);
+    }
+}
+int somatorio(int n){
+    if(n==0){
+        return 0;
+    }else{
+        return fibonacci(n)+somatorio(n-1);
+    }
+}
+main(){
+    int n;
+    printf("Digite um Numero Natural: ");
+    scanf("%d",&n);
+    printf("%d\n",somatorio(n));
+}
